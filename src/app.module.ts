1eeb7a931b249fc4f3dae95225802556265a4038
Bryan Patrick Coleman <totalknowledge@gmail.com>
2024-10-12 13:16:43 -0400

    Changes to nest endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,7 @@ import { Note } from './model/note';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: 'localhost',
+        host: 'postgres_bench',
         port: 5432,
         database: configService.get<string>('DATABASE'),
         username: configService.get<string>('DB_USER'),
